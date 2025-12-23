fd5cc49b883cf2d72e90883359204bc2dde1fe0d
Najwa Karima H. <karimanajwa.9040@gmail.com>
2025-12-24 00:03:56 +0800

    Created branch JournalPage and class JournalPage

---
+++ JournalPage.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+public class JournalPage{
+    private User user;
+
+    public JournalPage(User user) {
+        this.user = user;
+    }
+    
+    public void displayDates() {
+    }
+
+    public void createJournal(LocalDate date) {
+    }
+
+    public void viewJournal(LocalDate date) {
+    }
+
+    public void editJournal(LocalDate date) {
+    }
+}
