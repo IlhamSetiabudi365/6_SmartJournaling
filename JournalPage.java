fdd17ea2cd7a703cf24786ebc27616e191040bce
Najwa Karima H. <karimanajwa.9040@gmail.com>
2025-12-29 10:32:58 +0800

    Updated displayDates method, and created 2 additional methods

--- JournalPage.java
+++ JournalPage.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -14,6 +15,7 @@ public class JournalPage{
         LocalDate today = LocalDate.now();
 
         //calculate and diplay dates from 4 days ago up to and including today
+        System.out.println("=== Journal Dates ===");
         for(int i=4 , j=1 ; i>=0 ; i-- , j++){
             LocalDate dateToShow = today.minusDays(i);
             System.out.print(j + "." + dateToShow);
@@ -25,8 +27,40 @@ public class JournalPage{
 
         //prompt user
         System.out.print("\nSelect a date to view journal, or create a new journal for today: \n>");
-        int option = sc.nextInt();
-        LocalDate chosenDate = today.minusDays(5-option);
+        int choice = sc.nextInt();
+        LocalDate chosenDate = today.minusDays(5-choice);
+        showDateActions(chosenDate);
+    }
+    
+    //helper method to check if a file exists for that date
+    public boolean doesJournalExist(LocalDate date){
+        String path = "./journals/"+ user.getEmail() + "/" + date + ".txt";
+        File file = new File(path);
+        return file.exists();
+    }
+    
+    public void showDateActions(LocalDate chosenDate){
+        Scanner sc = new Scanner(System.in);
+
+        if (doesJournalExist(chosenDate)){
+            //if a journal exists(true), user can VIEW or EDIT
+            System.out.println("\nJournal found for " + chosenDate);
+            System.out.println("1. View Entry");
+            System.out.println("2. Edit Entry");
+            System.out.print("Choice: ");
+            int choice = sc.nextInt();
+        
+            if (choice == 1) viewJournal(chosenDate);
+            else if (choice == 2) editJournal(chosenDate);
+        }else{
+            //if a journal doesn't exist(false), user can only CREATE
+            System.out.println("\nNo journal found for " + chosenDate);
+            System.out.println("1. Create New Entry");
+            System.out.print("Choice: ");
+            int choice = sc.nextInt();
+        
+            if (choice == 1) createJournal(chosenDate);
+        }
     }
 
     public void createJournal(LocalDate date) {
